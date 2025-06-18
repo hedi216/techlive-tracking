3126e51d7efaff683ebb9ee8f4bf87fe659ce343
hedi216 <hedi216@gmail.com>
2025-06-19 00:28:50 +0100

    Fix: bind to 0.0.0.0 for Render

--- public_api/app_tracking.py
+++ public_api/app_tracking.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 import mysql.connector
+import os
 
 app = Flask(__name__, template_folder='templates')
 
@@ -45,4 +46,5 @@ def track():
     conn.close()
 
 if __name__ == "__main__":
-    app.run(port=8000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
