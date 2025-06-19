45b5af3ce8b12445432cd55922d356a5bee64f14
hedi216 <hedi216@gmail.com>
2025-06-19 02:51:18 +0100

    flask again ?

--- public_api/app_tracking.py
+++ public_api/app_tracking.py
@@ -2,7 +2,8 @@ from flask import Flask, request, jsonify, render_template
 #import mysql.connector
 import os
 import psycopg2
-import psycopg2.extras  
+from psycopg2.extras import RealDictCursor
+
 app = Flask(__name__, template_folder='templates')
 
 
@@ -31,6 +32,7 @@ def track():
         return jsonify({"error": "Numéro de téléphone invalide."})
 
     conn = get_connection()
+    cursor = conn.cursor(cursor_factory=RealDictCursor)
     cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
     if code:
